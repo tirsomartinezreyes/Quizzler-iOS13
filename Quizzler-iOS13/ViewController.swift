948b1527f61fb4894dfed0d4bf93e7b0edffe0da
tirsomartinezreyes <tirsomartinezreyes@gmail.com>
2021-10-24 21:05:11 -0500

    Changing bundle id,  product name, signature configuration, IB outlets, ib actions, string to boolean function and initialize with a question

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //IB Outlets
+    @IBOutlet weak var stackViewOutlet: UIStackView!
+    @IBOutlet weak var questionLabelOutlet: UILabel!
+    @IBOutlet weak var trueButtonOutlet: UIButton!
+    @IBOutlet weak var falseButtonOutlet: UIButton!
+    @IBOutlet weak var progressBarOutlet: UIProgressView!
+    
+    //IB Actions
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let response = sender.currentTitle!
+        let responseBolean = stringToBoolean(response)
+        print("answer:\(response) - \(responseBolean)")
+    }
+    
+    //Functions
+    func changeQuestion(){
+        questionLabelOutlet.text = "Does Tony stark the best avenger?"
+    }
+    
+    func stringToBoolean(_ val : String) -> Bool{
+        if(val == "True" || val == "true"){
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("on viewDidLoad")
+        changeQuestion()
     }
 
 
