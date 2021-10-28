42aa0c3ea2bb634f40cf677151aaf0c918e947bc
tirsomartinezreyes <tirsomartinezreyes@gmail.com>
2021-10-28 01:40:52 -0500

    Color feedback on buttons

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -11,23 +11,30 @@ import UIKit
 class ViewController: UIViewController {
     //Structures
     struct Question {
-        let question: String
-        let answer: Bool
+        let q: String
+        let a: Bool
         
-        init(question: String, answer: Bool){
-            self.question = question
-            self.answer = answer
+        init(q: String, a: Bool){
+            self.q = q
+            self.a = a
         }
     }
     
     
     //variables
     var originalQuestions = [
-        Question(question: "Question 1", answer:true),
-        Question(question: "Question 2", answer:true),
-        Question(question: "Question 3", answer:true),
-        Question(question: "Question 4", answer:true),
-        Question(question: "Question 5", answer:true)
+        Question(q: "A slug's blood is green.", a: true),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: true),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: true),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: true),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: false),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: true),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: false),
+        Question(q: "Google was originally called 'Backrub'.", a: true),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: true),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: false),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: false),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: true)
     ]
     
     var questions:[Question] = []
@@ -48,15 +55,27 @@ class ViewController: UIViewController {
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         let response = sender.currentTitle!
         let responseBolean = stringToBoolean(response)
-        let rightAnswer = currentQuestion?.answer
+        let rightAnswer = currentQuestion?.a
+        
+        let originalColor = sender.titleColor(for: .normal)
+        trueButtonOutlet.isEnabled = false
+        falseButtonOutlet.isEnabled = false
         
         if rightAnswer == responseBolean {
             print("Correct answer")
             rightAnswersCounter += 1
+            sender.setTitleColor(UIColor.green, for: .normal)
         }else{
             print("Wrong answer")
+            sender.setTitleColor(UIColor.red, for: .normal)
         }
-        changeQuestion()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2 ){
+            sender.setTitleColor(originalColor, for: .normal)
+            self.trueButtonOutlet.isEnabled = true
+            self.falseButtonOutlet.isEnabled = true
+            self.changeQuestion()
+        }
     }
     
     @IBAction func resetButtonPresses(_ sender: UIButton) {
@@ -72,7 +91,7 @@ class ViewController: UIViewController {
         let percentage =  (1.0 - Float(questions.count) / Float(numQuestions))
         
         if currentQuestion != nil {
-            questionLabelOutlet.text = currentQuestion?.question
+            questionLabelOutlet.text = currentQuestion?.q
         } else {
             finishedQuiz()
         }
